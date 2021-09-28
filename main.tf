7deee812a49e3001721bd3d0113e848e8d2a8974
ProsperSeb <prospersebille@gmail.com>
2021-09-28 17:07:14 +0200

    modified main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.65"
+      version = "2.65"
     }
   }
 
@@ -16,10 +16,10 @@ provider "azurerm" {
 
 
 resource "azurerm_policy_definition" "policy" {
-  name                  = "audit-automation-account-variable-encryption"
-  policy_type           = "Custom"
-  mode                  = "Indexed"
-  display_name          = " Audit Automation Account Variable Encrypion"
-  policy_rule           = file(".\Policies\AzureAutomationAccountVariablesEncryption\policy-rule.json")
-  parameters            = file(.\"Policies\AzureAutomationAccountVariablesEncryption\policy-parameters.json")
+  name = "audit-automation-account-variable-encryption"
+  policy_type = "Custom"
+  mode = "Indexed"
+  display_name = " Audit Automation Account Variable Encrypion"
+  policy_rule = file(".\Policies\AzureAutomationAccountVariablesEncryption\policy-rule.json")
+  parameters = file(.\"Policies\AzureAutomationAccountVariablesEncryption\policy-parameters.json")
 }
